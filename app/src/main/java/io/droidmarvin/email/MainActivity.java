33b07030f6ce7b68833298a2e8e6abd3327d5e49
ngesa marvin <marvinngesa@gmail.com>
2018-02-08 02:13:41 +0300

    initialize and grab from layouts

--- app/src/main/java/io/droidmarvin/email/MainActivity.java
+++ app/src/main/java/io/droidmarvin/email/MainActivity.java
@@ -2,12 +2,26 @@ package io.droidmarvin.email;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sendEmailButton;
+    EditText emailAddress;
+    EditText emailSubject;
+    EditText message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //grab from layout
+        emailAddress =  findViewById(R.id.email);
+        emailSubject = findViewById(R.id.subject);
+        message =  findViewById(R.id.message);
+        sendEmailButton =  findViewById(R.id.send_button);
     }
 }
