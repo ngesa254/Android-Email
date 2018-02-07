998075e4cebfb96a2e77c3db87df411affd34bbb
ngesa marvin <marvinngesa@gmail.com>
2018-02-08 02:15:09 +0300

    create onclick listener on the button

--- app/src/main/java/io/droidmarvin/email/MainActivity.java
+++ app/src/main/java/io/droidmarvin/email/MainActivity.java
@@ -1,7 +1,9 @@
 package io.droidmarvin.email;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
         emailSubject = findViewById(R.id.subject);
         message =  findViewById(R.id.message);
         sendEmailButton =  findViewById(R.id.send_button);
+
+        sendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String toemailAddress = emailAddress.getText().toString();
+                String msubject = emailSubject.getText().toString();
+                String mmessage = message.getText().toString();
+
+                Intent emailApp = new Intent(Intent.ACTION_SEND);
+                emailApp.putExtra(Intent.EXTRA_EMAIL, new String[]{toemailAddress});
+                emailApp.putExtra(Intent.EXTRA_SUBJECT, msubject);
+                emailApp.putExtra(Intent.EXTRA_TEXT, mmessage);
+                emailApp.setType("message/rfc822");
+                startActivity(Intent.createChooser(emailApp, "Send Email Via"));
+
+            }
+        });
+
     }
 }
